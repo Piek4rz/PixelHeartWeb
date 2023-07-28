0f10976b2c973fdb12fddb8061a796f0f27c8e38
Jakub Piekarski <jakubp2000@gmail.com>
2023-07-28 19:45:28 +0200

    edit profile and list

--- src/Pages/ProfileSubpages/List.jsx
+++ src/Pages/ProfileSubpages/List.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import "../../Components/Background/Profile/List.css"
+import "../../Components/Background/Profile/List.css";
+import p1 from "../../Assets/person1.png"
 
 const List = () => {
     return (
-        <div>
+        <div className="listContainer">
                 <div className="profilesList">
-
                     <div className="profileCard">
                         <div className="cardContents">
-                            <p>someone</p>
+                            <img src={p1}/>
+                            <div className="cardText">
+                                <p>someone</p>
+                                <p className="message">chat message chat message </p>
+                            </div>
                         </div>
                     </div>
                     <div className="profileCard">
@@ -21,7 +25,23 @@ const List = () => {
                     <div className="profileCard">
                         <p>someone</p>
                     </div>
+                    <div className="profileCard">
+                        <p>someone</p>
+                    </div>
+                    <div className="profileCard">
+                        <p>someone</p>
+                    </div>
+                    <div className="profileCard">
+                        <p>someone</p>
+                    </div>
+                    <div className="profileCard">
+                        <p>someone</p>
+                    </div>
                 </div>
+            <div className="chatContainer">
+                chat
+                <p>tba..</p>
+            </div>
         </div>
     );
 };
